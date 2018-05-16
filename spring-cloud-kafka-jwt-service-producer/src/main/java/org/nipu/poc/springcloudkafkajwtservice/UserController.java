5aa7503874e5b1f8ef14a15ce055bd10da827bdf
= <=>
2018-05-16 15:32:04 +0400

    Send encoded JWT and decode on consumer side

--- spring-cloud-kafka-jwt-service-producer/src/main/java/org/nipu/poc/springcloudkafkajwtservice/UserController.java
+++ spring-cloud-kafka-jwt-service-producer/src/main/java/org/nipu/poc/springcloudkafkajwtservice/UserController.java
@@ -50,10 +50,11 @@ public class UserController {
         }
         OAuth2AuthenticationDetails authenticationDetails = (OAuth2AuthenticationDetails) ((OAuth2Authentication) principal).getDetails();
         log.info("Authentication Details is {}", authenticationDetails);
-        log.info("Token Value is {}", authenticationDetails.getTokenValue());
-        Jwt jwt = JwtHelper.decode(authenticationDetails.getTokenValue());
+        final String tokenValue = authenticationDetails.getTokenValue();
+        log.info("Token Value is {}", tokenValue);
+        Jwt jwt = JwtHelper.decode(tokenValue);
         log.info("JWT is {}", jwt);
-        sender.send(MESSAGING_TOPIC, new MessageContainer<>(String.valueOf(jwt)));
+        sender.send(MESSAGING_TOPIC, new MessageContainer<>(String.valueOf(tokenValue)));
     }
 
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
--- spring-cloud-kafka-jwt-service/src/main/java/org/nipu/poc/springcloudkafkajwtservice/messaging/Receiver.java
+++ spring-cloud-kafka-jwt-service/src/main/java/org/nipu/poc/springcloudkafkajwtservice/messaging/Receiver.java
@@ -3,6 +3,8 @@ package org.nipu.poc.springcloudkafkajwtservice.messaging;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -15,6 +17,8 @@ public class Receiver {
     @KafkaListener(topics = "${spring.kafka.topic.messaging}")
     public void receive(MessageContainer payload) {
         LOGGER.info("received payload='{}'", payload);
+        final Jwt jwt = JwtHelper.decode((String) payload.getMessage());
+        LOGGER.info("Jwt decoded: {}", jwt);
         latch.countDown();
     }
 }
